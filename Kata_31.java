fa3a06a25783314bdfb9533ad443acd7e719dc55
Heeeun9812 <heeeun9812@gmail.com>
2024-01-05 10:06:52 +0900

    2024/01/05

---
+++ Kata_31.java
@@ -0,0 +1,21 @@
+// 길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
+
+import java.util.ArrayList;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        ArrayList<String> arrList = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 2 != 0){
+                arrList.add("수");
+            }else{
+                arrList.add("박");
+            }
+        }
+        for(String s: arrList){
+            answer+=s;
+        }
+        return answer;
+    }
+}
